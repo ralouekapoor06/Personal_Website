9204edd7567e742d2d3ff46c86b416343d541677
raloue1 <ralouekapoor1@gmail.com>
2020-05-14 14:22:36 +0530

    Added port

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+const PORT = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(express.static("public")); 
 app.get('/', function (req, res) {
   res.render('index');
 });
-app.listen(3000, function () {
-  console.log("Server Started");
+app.listen(PORT, function() {
+  console.log(`Server started on port ${PORT}`)
 });
